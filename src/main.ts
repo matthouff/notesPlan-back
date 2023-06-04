72dd103d415d2b59eb2da619ec83c891bb1e753d
matthiasberthelot <berthelotmatthias@gmail.com>
2023-06-04 02:58:11 +0200

    Ajout des cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // débloque la requête Cross-Origin (requête HTTP effectuée depuis un domaine, un protocole ou un port différent de celui de la ressource demandée)
+  const corsOptions: CorsOptions = {
+    origin: true, // Remplacez true par l'URL ou les origines autorisées
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+  app.use(cors(corsOptions));
+
   await app.listen(3000);
 }
 bootstrap();
